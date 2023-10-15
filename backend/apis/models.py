418ea6b816b3c451a1b3388d3cbf0144389acc41
Rujith951 <rujithramu3951@gmail.com>
2023-10-15 11:04:04 -0700

    added sqlite to gitignore, routerdom, models.

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Products(models.Model):
+    category_choices = [
+        ("FURNITURE", "FURNITURE"),
+        ("ELECTRICALS", "ELECTRICALS"),
+        ("SPORTS", "SPORTS"),
+    ]
+    p_name = models.CharField(max_length=100, null=False, blank=False)
+    p_category = models.CharField(
+        choices=category_choices, null=False, blank=False, max_length=100
+    )
+    vendor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Trasactions(models.Model):
+    t_date = models.DateField(null=False)
+    t_amount = models.IntegerField(null=False)
+    vendor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
